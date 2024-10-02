f284683fd6547a665724052dfe82c267bc1e62b9
anonimuxxxz <antobailser@gmail.com>
2024-10-02 11:10:32 +0200

    Add files via upload

---
+++ src/test/java/antonio/jenkies/archivo/ArchivoApplicationTests.java
@@ -0,0 +1,13 @@
+package antonio.jenkies.archivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
